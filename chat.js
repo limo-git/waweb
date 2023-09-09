9cadf0c76d132b345515585c6a86cff2b6d1464b
limo-git <130894560+limo-git@users.noreply.github.com>
2023-09-09 16:26:31 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,15 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+    const pictureUpload = document.getElementById("picture-upload");
+    const profilePicture = document.getElementById("profile-picture");
+  
+    pictureUpload.addEventListener("change", function(e) {
+      const file = e.target.files[0];
+      if (file) {
+        const imageUrl = URL.createObjectURL(file);
+        profilePicture.src = imageUrl;
+      }
+    });
+  }); */
+
+
+  
---
+++ profile.js
@@ -0,0 +1,3 @@
+.onlcick = function() {
+    
+}
